505b78b77ea8b2d21dad7bdea0cbb8a655ac62b6
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-17 02:59:23 +0700

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+    let push = true;
+    let arr = [];
+    for(let i = 0; i < array.length; i++){
+        for (const item of element){
+            if(array[i] === item){
+                push = false;
+            }
+        }
+        if(push){
+            arr.push(array[i]);
+        }
+        push = true;
+    }
+    return arr;
 };
 
 // Do not edit below this line
