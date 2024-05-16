bb291f20f39007434b6d005f4fcc52c192f9cdda
Tanavat-L <102414255+Tanavat-L@users.noreply.github.com>
2024-05-17 03:08:32 +0700

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1 < 0 || num2 < 0 || typeof(num1) !== typeof(1) || typeof(num2) !== typeof(1)) return "ERROR";
+    if(num1 > num2){
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for(let i = num1; i <= num2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
